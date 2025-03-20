4e569a1160abb63ac43fa92bf5489e501fd835ba
Jeanne <jeanne.bnvt@gmail.com>
2025-03-21 00:07:19 +0100

    discretisation dans l'env , pas de bug mais test à faire

--- src/env.py
+++ src/env.py
@@ -2,21 +2,38 @@
 import numpy as np
 
 from MPRengine import Board
-
+from math import prod
 
 #Mad Pod Racing Environnement
 class MPR_env():
 
-    def __init__(self):
+    def __init__(self, discretisation : list, nb_action):
+
         self.board = Board(3,3)
         self.terminated = False
-        self.HEIGHT,self.WIDTH = self.board.getInfos()
+        height, width = self.board.getInfos()
+        
+        #discretisation est une liste qui donne le nombre de valeurs possible pour chaque attribut que l'on souhaite discretiser 
+        #ex si etat = (angle,distance,vitesse) et angle peut prendre 7 valeurs distance 4 et vitesse 3 alors discretisation = [7,4,3]
+
+        self.discretisation = discretisation
+
+        #on stock position precedente pour deriver la vitesse, past_pos = (x,y)
+        self.past_pos= self.board.pod.getCoord()
+
+        self.max_dist = np.sqrt(width**2+height**2)
+        
+        self.nb_action = nb_action
+        self.nb_etat = prod([discretisation[0]+2] + discretisation[1:])
+        # le plus 2 est pas propre mais c'est pour la discretisation de l'angle on choisit 
+        # step de discretisation pour les angles devant auquel on ajoute 2 pour les 2 etat possible si angle derriere
         
 
 
         
-    def step(self,  thrust):
+    def step(self,  action):
         next_cp = self.board.checkpoints[self.board.next_checkpoint]
+        thrust = self.convert_action(action)
         x,y,next_cp_x,next_cp_y,dist,angle = self.board.play(next_cp,thrust)
 
         #si rien de specifique ne s'est produit 
@@ -38,23 +55,56 @@ class MPR_env():
         if dist<600:
             reward = 50
 
-        return x,y,next_cp_x,next_cp_y,dist,angle,reward, self.terminated
+        return self.discretized_state(angle, dist, x,y),reward, self.terminated
     
 
     def reset(self,seed=None,options=None):
-        self.board = Board(3,3)
+        self.board = Board(nb_cp=3,nb_round=3)
         self.terminated = False
 
         x, y = self.board.pod.getCoord()
+        self.past_pos= (x,y)
 
         next_cp = self.board.checkpoints[self.board.next_checkpoint]
         cp_x, cp_y = next_cp.getCoord()
         dist = self.board.pod.distance(next_cp)
         angle = self.board.pod.angle
 
-        return x,y,cp_x,cp_y,dist,angle
+        return self.discretized_state(angle,dist, x, y)
+    
+
+    def discretized_angle(self, angle):
+        #discretisation de l'angle self.discretisation corresponds à en combien d'etats on discretise un angle qui 
+        #corresponds à devant le pod. si l'angle indique l'arrière du pod il est discretisé en deux états
+        if 0<= angle<= 180:
+            for i in range(self.discretisation[0]):
+                if angle <  (i+1)* (180/self.discretisation[0]):
+                    return i
+        if angle < 270:
+            return self.discretisation[0]
+        else:
+            return self.discretisation[0] +1
+        
+    def discretized_distance(self, dist):
+        if dist> self.max_dist:
+            dist= self.max_dist
+        return round(dist/self.max_dist * self.discretisation[1])
+    
+    def discretized_speed(self, x,y):
+
+        vitesse = np.sqrt((x - self.past_pos[0])**2 + (y - self.past_pos[1])**2)
+        return round(vitesse/self.max_dist * self.discretisation[2])
 
 
+    def discretized_state(self, angle, dist, x, y):
+        state = (self.discretized_angle(angle), self.discretized_distance(dist), self.discretized_speed(x,y))
+        self.past_pos = (x,y)
+        index = state[0]*(self.discretisation[1] * self.discretisation[2]) + state[1]*self.discretisation[2] + state[2]
+        # print(state, index)
+        # print(f"{state[0]}*({self.discretisation[1]} * {self.discretisation[2]}) + {state[1]}*{self.discretisation[2]} + {state[2]}")
+        return index-1
 
+    def convert_action(self, action):
+        return action* (100/self.nb_action)
 
 
--- src/qagent.py
+++ src/qagent.py
@@ -8,72 +8,82 @@ import csv
 class Qagent:
     def __init__(self, env, episodes, max_steps,alpha = .5, epsilon = .3, gamma = 1, d_dist =300,d_angle=36, d_thrust=30):
         self.env= env
-        self.d_dist = d_dist
-        self.d_angle = d_angle
-        self.d_thrust = d_thrust
+        # self.d_dist = d_dist
+        # self.d_angle = d_angle
+        # self.d_thrust = d_thrust
         self.episodes = episodes
         self.max_steps = max_steps
         self.alpha = alpha
         self.epsilon = epsilon
         self.gamma = gamma   
-        width, height = self.env.board.getInfos()
+        # width, height = self.env.board.getInfos()
 
-        self.max_dist = np.sqrt(width**2+height**2)
+        # self.max_dist = np.sqrt(width**2+height**2)
         
-        self.qtable = np.zeros((self.d_dist,self.d_angle,self.d_thrust))
+        self.qtable = np.zeros((self.env.nb_etat,self.env.nb_action))
         self.rewards = []
 
     def train(self):
         for i in tqdm(range(self.episodes)):
-            x,y,cp_x,cp_y,dist,angle = self.env.reset()
-            state = self.discretized_state((dist,angle))
+            state = self.env.reset()
+            # state = self.discretized_state((dist,angle))
             for j in range(self.max_steps):
                 action = self.epsilon_greedy(state)
-                x,y,next_cp_x,next_cp_y,dist,angle,reward,terminated = self.env.step(action*self.d_thrust)
-                next_state = self.discretized_state((dist,angle))
-                self.update_q_table(state,action-1,next_state,reward)
+                next_state,reward,terminated = self.env.step(action)
+                # next_state = self.discretized_state((dist,angle))
+                self.update_q_table(state,action,next_state,reward)
                 state = next_state
                 if terminated:
+                    # print(j)
                     break
-            self.test()
-                    
+            # self.test()
+
     def test(self):
-        x,y,cp_x,cp_y,dist,angle = self.env.reset()
-        b_x= [b.getCoord()[0] for b in self.env.board.checkpoints]
-        b_y= [b.getCoord()[1] for b in self.env.board.checkpoints]
-
-        l_x = [x]
-        l_y=[y]
-        state = self.discretized_state((dist,angle))
-
-        cum_reward = 0
+        state = self.env.reset()
         for j in range(self.max_steps):
-            action = np.argmax(self.qtable[state[0], state[1], :])
-            x,y,next_cp_x,next_cp_y,dist,angle,reward,terminated = self.env.step(action*self.d_thrust)
-            l_x.append(x)
-            l_y.append(y)
-            next_state= self.discretized_state((dist,angle))
-            cum_reward+= reward
+            action = self.epsilon_greedy(state)
+            next_state,reward,terminated = self.env.step(action)
+            state = next_state
             if terminated:
                 break
-            state = next_state
+                    
+    # def test(self):
+    #     x,y,cp_x,cp_y,dist,angle = self.env.reset()
+    #     b_x= [b.getCoord()[0] for b in self.env.board.checkpoints]
+    #     b_y= [b.getCoord()[1] for b in self.env.board.checkpoints]
 
-        plt.figure()
-        plt.scatter(l_x,l_y,c =np.arange(len(l_x)), s = 1)
-        plt.scatter(b_x,b_y, c = 'red', s=600)
-        # print(cum_reward)
-        self.rewards.append(cum_reward)
-        # plt.show()
-        plt.close()
+    #     l_x = [x]
+    #     l_y=[y]
+    #     state = self.discretized_state((dist,angle))
+
+    #     cum_reward = 0
+    #     for j in range(self.max_steps):
+    #         action = np.argmax(self.qtable[state[0], state[1], :])
+    #         x,y,next_cp_x,next_cp_y,dist,angle,reward,terminated = self.env.step(action*self.d_thrust)
+    #         l_x.append(x)
+    #         l_y.append(y)
+    #         next_state= self.discretized_state((dist,angle))
+    #         cum_reward+= reward
+    #         if terminated:
+    #             break
+    #         state = next_state
+
+    #     plt.figure()
+    #     plt.scatter(l_x,l_y,c =np.arange(len(l_x)), s = 1)
+    #     plt.scatter(b_x,b_y, c = 'red', s=600)
+    #     # print(cum_reward)
+    #     self.rewards.append(cum_reward)
+    #     # plt.show()
+    #     plt.close()
 
     def epsilon_greedy(self,state):
         if np.random.random() < self.epsilon:
-            return np.random.randint(0,self.d_thrust)
-        return np.argmax(self.qtable[state[0], state[1], :])
+            return np.random.randint(0,self.env.nb_action)
+        return np.argmax(self.qtable[state])
     
     def update_q_table(self, state, action, next_state, reward):
-        next_q = np.max(self.qtable[tuple(next_state)])
-        self.qtable[tuple(state)+(action,)] += self.alpha*(reward + self.gamma*next_q - self.qtable[tuple(state)+(action,)])
+        next_q = np.max(self.qtable[next_state])
+        self.qtable[state, action] += self.alpha*(reward + self.gamma*next_q - self.qtable[state,action])
 
     
     def discretized_state(self,state):
@@ -97,8 +107,9 @@ class Qagent:
 
 
 def main():
-    agent = Qagent(MPR_env(),5000,1000)
+    env = MPR_env(discretisation=[7,4,3], nb_action=4)
+    agent = Qagent(env,5000,1000)
 
     agent.train()
     agent.test()
-# main()
+main()
