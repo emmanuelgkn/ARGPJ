d82c6cf7c9d64c3bac6eb35e719369167a1d5b01
Emmanuel Gokana <emmanuelgokana@gmail.com>
2025-05-20 23:37:38 +0200

    modifs creation graphes

--- src/QNN/envnn.py
+++ src/QNN/envnn.py
@@ -41,7 +41,7 @@ class MPR_envnn():
         #effectuer action
         next_cp = self.board.checkpoints[self.board.next_checkpoint]
         target_x, target_y, thrust = self.convert_action(action)
-        x,y,_,_,dist,angle = self.board.play(Point(target_x,target_y),thrust) #Point(target_x,target_y)
+        x,y,_,_,dist,angle = self.board.play(next_cp,thrust) #Point(target_x,target_y)
 
         #calcul pour l'état
         vitesse = self.compute_speed(x,y)
@@ -111,21 +111,21 @@ class MPR_envnn():
 
 
     def convert_action(self, action):
-        # mapping_thrust = {0: 0, 1: 70, 2: 100}
-        mapping_thrust= {0:0, 1:25, 2:50,3:70,4: 80, 5:100}
-        thrust = mapping_thrust[action // 7]
-        # mapping_angle = {0: -90,1:-45, 2: 0, 3:45, 4: 90}
-        mapping_angle = {0: -18,1:-9, 2: -3, 3:0, 4: 3, 5:9,6:18}
+        mapping_thrust = {0: 0, 1: 50, 2: 100}
+        thrust = mapping_thrust[action // 5]
+        mapping_angle = {0: -18,1:-9, 2: 0, 3:9, 4: 18}
+
         x_past, y_past = self.past_pos
         x,y = self.current_pos
 
-        angle_action = mapping_angle[action % 7]
-        angle = math.degrees(math.atan2(y-y_past, x-x_past))
-        
+        angle_action = mapping_angle[action % 5]
+        angle =math.degrees(math.atan2(y-y_past, x-x_past))
+
         new_angle = (angle + angle_action +540)%360 -180
-        new_x = x + math.cos(math.radians(new_angle)) *500
-        new_y = y + math.sin(math.radians(new_angle)) *500
-        return int(round(new_x)), int(round(new_y)), thrust
+        new_x = x + math.cos(math.radians(new_angle)) *1000
+        new_y = y + math.sin(math.radians(new_angle)) *1000
+        
+        return int(new_x), int(new_y), thrust
 
     def reset(self):
         self.board = Board(nb_cp=self.nb_cp,nb_round=self.nb_round,custom =self.custom)
